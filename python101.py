33866ce658db8e133305fa3254bea27da3c4f39d
Guido Bacce <gbacce@MacBook-Pro.local>
2017-04-18 11:28:53 -0400

    This is my first commit

---
+++ python101.py
@@ -0,0 +1,16 @@
+# print "Hello, world!"
+
+print ("Hello world!", "Again")
+
+# This won't work!!
+# print "This
+# is a couple
+# lines of a sentence.
+# Please print.
+
+
+# This will...
+print """This
+will ignore the new lines
+until it sees another
+three double quotes"""
